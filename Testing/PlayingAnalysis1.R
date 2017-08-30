4c95273f0fdea490e3ef57b462c01665b1ebaa02
Paul Bramley <paul.bramley@bristol.ac.uk>
2017-08-30 17:02:13 +0100

    Controlled RNG. Altered sample size distribution

--- Testing/PlayingAnalysis1.R
+++ Testing/PlayingAnalysis1.R
@@ -33,14 +33,14 @@ registerDoParallel(c1)
 
 r <- foreach (m = 1:Reps, 
               .combine=rbind, .packages = c("data.table"), 
-              .export = c("Studies", "Subj", "controlProp", "theta", "tau.sq", "EvFreq", "Normal.Simulation")
+              .export = c("Studies", "Subj", "controlProp", "theta", "tau.sq", "True.sd", "Normal.Simulation")
 ) %dopar% {
   
   ID.this.loop <- integer()
   
   for (i in Subj){
     
-    for (j in EvFreq){
+    for (j in True.sd){
       
       for (k in theta){
         
@@ -66,18 +66,22 @@ r <- foreach (m = 1:Reps,
   
   ID <- length(ID.this.loop)
   
-  Normal.Simulation.Results <- data.table(
+  Normal.Sim.Results <- data.table(
     Unique_ID = c(ID.this.loop),
     Rep_Number = integer(length = ID),
     Rep_Subj = numeric(length = ID),
     Rep_sd = numeric(length = ID),
     Rep_theta = numeric(length = ID),
     Rep_tau.sq = numeric(length = ID),
-    Rep_NumStudies = numeric(length = ID)
-#     Study_ID = integer(length = ID),
-#     Study_estimate = numeric(length = ID),
-#     Study_sd = numeric(length = ID),
-#     Study_n = integer(length = ID)
+    Rep_NumStudies = numeric(length = ID),
+    FE_Estimate = numeric(length = ID),
+    FE_Est_Low_CI = numeric(length = ID),
+    FE_Est_Up_CI = numeric(length = ID),
+    REML_Estimate = numeric(length = ID),
+    REML_Est_Low_CI = numeric(length = ID),
+    REML_Est_Up_CI = numeric(length = ID),
+    REML_tau2 = numeric(length = ID),
+    REML_I2 = numeric(length = ID)
   )
   
   for (i in Subj){
@@ -93,15 +97,23 @@ r <- foreach (m = 1:Reps,
             ### Temporary data.table
             temp.data <- Normal.Simulation[J(m, i, j, k, l, n)]
             
-            meta.analysis.fe <- rma.uni(yi = temp.data$Study_estimate, vi = temp.data$Rep_sd^2, method = "FE")
+            ma.fe <- rma.uni(yi = temp.data$Study_estimate, vi = temp.data$Rep_sd^2, method = "FE")
             
-            meta.analysis.reml <- rma.uni(yi = temp.data$Study_estimate, vi = temp.data$Rep_sd^2, method = "REML")
+            ma.reml <- rma.uni(yi = temp.data$Study_estimate, vi = temp.data$Rep_sd^2, method = "REML")
+            
+            Normal.Sim.Results[Unique_ID == counter, `:=` (Rep_Number= m, Rep_Subj = i, Rep_sd = j,
+                                                          Rep_theta = k, Rep_tau.sq = l, Rep_NumStudies = n,
+                                                          FE_Estimate = ma.fe[1],
+                                                          FE_Est_Low_CI = ma.fe[5],
+                                                          FE_Est_Up_CI = ma.fe[6],
+                                                          REML_tau2 = ma.reml[8],
+                                                          REML_I2 = ma.reml[22])]
             
           }
         }
       }
     }
   }
-  Normal.Simulation.Results
+  Normal.Sim.Results
 }
             
--- Testing/PlayingWithCurves.R
+++ Testing/PlayingWithCurves.R
@@ -50,7 +50,7 @@ for (a in seq(0,1, 0.01) ){
     #for (b in c(1, 0.6, 0.4, 0.3, 0.2, 0.1, 0.05, 0.03, 0.01)){  
     
     #Begg_weight <- 0.6 *  (1 - (1/ (b^0.5) * (a )  ))   + 0.4 / (1 +  (a / 0.07)^10)
-    Begg_weight <- 0.5 *(1 - (1/ ( sqrt(b))  ))  +  0.5 / ( 1 + exp(- 100 * -(a-0.06) )) ^ (1/1)
+    Begg_weight <- 0.5 *(1 - (1/ ( b ^0.2) *sqrt(a) ))  +  0.5 / ( 1 + exp(- 100 * -(a-0.06) )) ^ (1/1)
     
     dummy1 <- append(dummy1, a)
     dummy2 <- append(dummy2, Begg_weight)
@@ -62,4 +62,19 @@ for (a in seq(0,1, 0.01) ){
 plot(dummy1, dummy2, ylim = c(0,1))
 abline(v = 0.05)
 
-plot(dummy1, dummy2)
+### likelihood of getting bias based on sample size
+
+dummy1 <- numeric()
+dummy2 <- numeric()
+
+for (a in seq(0,1000, 1)){
+  Weight2 <- 1/(exp(a^0.15))
+  
+  dummy1 <- append(dummy1, a)
+  dummy2 <- append(dummy2, Weight2)
+  
+}
+
+plot(dummy1, dummy2, ylim = c(0,1))
+
+mean(dummy2)
